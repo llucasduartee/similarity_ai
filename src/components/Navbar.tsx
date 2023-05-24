f1e02a0a5f7bcbdf83efd316569e532507aac188
Lucas Duarte Coelho <lucas@loldesign.com.br>
2023-05-24 20:23:59 -0300

    feat: adding api key and user dashboard

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,15 +5,16 @@ import { ThemeToggle } from "./ThemeToggle";
 import { buttonVariants } from "./ui/Button";
 import SignInButton from "./ui/SignInButton";
 import SignOutButton from "./ui/SignOutButton";
+import { authOptions } from "@/lib/auth";
 
 const Navbar = async () => {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
 
   return (
     <div className="fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between">
       <div className="container max-w-7xl mx-auto w-full flex justify-between items-center">
         <Link href="/" className={buttonVariants({ variant: "link" })}>
-          Text Similarity v1.0
+          Similarity API v1.2
         </Link>
 
         <div className="md:hidden">
